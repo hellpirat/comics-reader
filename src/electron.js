036645ba9193af5755a6c31024ce3c4623c7b3ea
Evgenii Maltsev <hellpirat1@gmail.com>
2021-08-28 14:26:25 +0300

    node integration

--- src/electron.js
+++ src/electron.js
@@ -8,7 +8,9 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
+      // https://www.electronjs.org/docs/tutorial/security#isolation-for-untrusted-content
       nodeIntegration: true,
+      contextIsolation: false,
       //   preload: path.join(__dirname, "preload.js"),
     },
   });
